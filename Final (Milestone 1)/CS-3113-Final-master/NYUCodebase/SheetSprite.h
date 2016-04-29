b3db8d0b7545718de5c3771e982b6e7329207a23
mpiazzola461 <mmp461@nyu.edu>
2016-04-28 23:02:06 -0400

    Update for 4/28
    Need to fix:
    - Bullets
    - Title Screen

---
+++ Final (Milestone 1)/CS-3113-Final-master/NYUCodebase/SheetSprite.h
@@ -0,0 +1,36 @@
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#ifndef SPRITESHEET_H
+#define SPRITESHEET_H
+
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include "ShaderProgram.h"
+
+//#ifdef _WINDOWS
+//#define RESOURCE_FOLDER ""
+//#else
+//#define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
+//#endif
+
+class SheetSprite{
+public:
+	//SheetSprite();
+	SheetSprite(ShaderProgram* program, unsigned int textureID, float u, float v, float width, float height, float size) :
+		program(program), textureID(textureID), u(u), v(v), width(width), height(height), size(size) {}
+	void Draw();
+	
+	ShaderProgram* program;
+	float size;
+	unsigned int textureID;
+	float u;
+	float v;
+	float width;
+	float height;
+};
+
+#endif
+
